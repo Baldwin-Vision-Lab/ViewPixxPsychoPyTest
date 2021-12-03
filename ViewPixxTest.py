1dff27be10c5df139c441f5c40cbde161bc75af5
Annabel Fan <annabel.fan@mail.mcgill.ca>
2021-12-03 11:46:09 -0500

    added rough colours for the squares

--- ViewPixxTest.py
+++ ViewPixxTest.py
@@ -11,10 +11,10 @@ win = visual.Window([800,600], fullscr=False, monitor="testMonitor", color="blac
 grating = psychopy.visual.GratingStim(win, units='cm', size=[5,5], tex='sin', pos=(0,5))
 
 
-rectangle1 = psychopy.visual.rect.Rect(win, units ='cm', fillColor=[105,105,105], fillColorSpace=None, pos=(-7.5, -2), size=[5,5], ori=0.0, opacity=None, contrast=1.0, colorSpace='rgb255')
-rectangle2 = psychopy.visual.rect.Rect(win, units ='cm', fillColor=[106,106,106], fillColorSpace=None, pos=(-2.5, -2), size=[5,5], ori=0.0, opacity=None, contrast=1.0, colorSpace='rgb255')
-rectangle3 = psychopy.visual.rect.Rect(win, units ='cm', fillColor=[], fillColorSpace=None, pos=(2.5, -2), size=[5,5], ori=0.0, opacity=None, contrast=1.0, colorSpace='rgb')
-rectangle4 = psychopy.visual.rect.Rect(win, units ='cm', fillColor=[], fillColorSpace=None, pos=(7.5, -2), size=[5,5], ori=0.0, opacity=None, contrast=1.0, colorSpace='rgb')
+rectangle1 = psychopy.visual.rect.Rect(win, units ='cm', fillColor=[129,129,129], fillColorSpace=None, pos=(-7.5, -2), size=[5,5], ori=0.0, opacity=None, contrast=1.0, colorSpace='rgb255')
+rectangle2 = psychopy.visual.rect.Rect(win, units ='cm', fillColor=[128,128,128], fillColorSpace=None, pos=(-2.5, -2), size=[5,5], ori=0.0, opacity=None, contrast=1.0, colorSpace='rgb255')
+rectangle3 = psychopy.visual.rect.Rect(win, units ='cm', fillColor=[0], fillColorSpace=None, pos=(2.5, -2), size=[5,5], ori=0.0, opacity=None, contrast=1.0, colorSpace='rgb')
+rectangle4 = psychopy.visual.rect.Rect(win, units ='cm', fillColor=[0.003], fillColorSpace=None, pos=(7.5, -2), size=[5,5], ori=0.0, opacity=None, contrast=1.0, colorSpace='rgb')
 
 rectangles = [rectangle1, rectangle2, rectangle3, rectangle4]
 
